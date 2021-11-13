1cb65e2561772586bef8e78b8cfb85b1516d2cb2
kayash <katya_kayashova@icloud.com>
2021-11-14 02:14:09 +0300

    Add plots

--- controllers/recommendations.go
+++ controllers/recommendations.go
@@ -1,10 +1,35 @@
 package controllers
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func Recommendations(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "It's reccomendations page!")
+type Product struct {
+	Name string
+	ID   int64
+}
+
+type RecommendationsData struct {
+	ProductList []Product
+}
+
+func Recommendations(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.
+		New("recommendations.html").
+		ParseFiles("resources/templates/recommendations.html")
+	if err != nil {
+		log.Fatal("Recommendations: "+err.Error(), err)
+	}
+	products := []Product{{Name: "kakashka", ID: 1}, {Name: "bukashka", ID: 2}, {Name: "kandibober", ID: 3}}
+
+	data := RecommendationsData{
+		ProductList: products,
+	}
+
+	tplErr := tmpl.Execute(w, data)
+	if tplErr != nil {
+		log.Fatal("SayHello: ", err)
+	}
 }
--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", controllers.SayHello) // Устанавливаем роутер
-	http.HandleFunc("/recommendations", controllers.Recommendations) // Устанавливаем роутер
+	http.HandleFunc("/", controllers.SayHello)                            // Устанавливаем роутер
+	http.HandleFunc("/recommendations/{id}", controllers.Recommendations) // Устанавливаем роутер
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./resources/css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./resources/img"))))
 	//http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./resources/js"))))
 
 	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера http (пока)
