f325208740149b703395ee997217dd13e6aad60d
kayash <katya_kayashova@icloud.com>
2021-11-13 22:06:24 +0300

    Add styles

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	http.HandleFunc("/", controllers.SayHello) // Устанавливаем роутер
 	http.HandleFunc("/recommendations", controllers.Recommendations) // Устанавливаем роутер
-
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./resources/css"))))
+	//http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./resources/js"))))
 
 	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера http (пока)
 	if err != nil {
